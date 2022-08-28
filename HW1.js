0feb5b4bfc65b62fe469d8877eb84ba881ec70a6
Екатерина Katrin <>
2022-08-28 15:36:30 +0000

    Initial commit

---
+++ HW1.js
@@ -0,0 +1,25 @@
+// 1.1 Объявите две переменные: firstName и lastName.
+let firstName =  "John "
+let lastName = "Adams" + '<br> '
+//1.2 Запишите строку “John”в переменную firstName и строку “Adams”  в переменную lastName.
+//1.3 Выведите на экран значение firstName и lastName (должно показать John Adams).
+document.write( firstName, lastName )
+//1.4 Используйте те же переменные firstName и lastName для того, чтобы вывести новое значение (Anna Karenina).
+firstName = "Anna "
+lastName = "Karenina"
+document.write( firstName, lastName, '<br>', '<br>')
+//1.5 Напишите алгоритм приготовления кофе и выведите его на экран
+
+let coffee = "Make coffee: <br> <br>" 
+let step_1 = "1. Take a kettle <br>"  
+let step_2 = "2. Turn on a kettle <br>"
+let step_3 = "3. Take some coffee <br>"
+let step_4 = "4. Take  a cup for coffee <br>"
+let step_5 = "5. Put coffee into the cup <br>"
+let step_6 = "6. Take  hot water from kettle <br> "
+let step_7 = "7. Put hot water into the cup <br>"
+let step_8 = "8. Take a milk <br>"
+let step_9 = "9. Put milk into the cup <br>"
+let step_10 = "10. Shuffle everything "
+
+document.write(coffee, step_1, step_2, step_3, step_4, step_5, step_6, step_7, step_8, step_9, step_10 )
